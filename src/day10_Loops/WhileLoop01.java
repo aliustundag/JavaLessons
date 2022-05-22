048f93133ec23d54fa1a35d0dc453f9e62329846
ali ustundag <aliokanustundag@gmail.com>
2022-05-22 16:54:52 +0100

    day10

---
+++ src/day10_Loops/WhileLoop01.java
@@ -0,0 +1,21 @@
+package day10_Loops;
+
+public class WhileLoop01 {
+
+    public static void main(String[] args) {
+
+        //Type code to print "Hi!" 5 times on the console by using for-loop
+        for(int i=1; i<6; i++) {
+            System.out.println("Hi!");
+        }
+
+        System.out.println("=============");
+
+        //Type code to print "Hi!" 5 times on the console by using while-loop
+        int i=1;
+        while(i<4) {
+            System.out.println("Hi!");
+            i++;//If you forget increment or decrement inside the while-loop body you will have infinite loop.
+        }
+    }
+}
