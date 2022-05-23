f575badd08b69bbaeeffaa7b47f694249debb273
ali ustundag <aliokanustundag@gmail.com>
2022-05-23 12:03:30 +0100

    day10

---
+++ src/day10_Loops/WhileLoop03.java
@@ -0,0 +1,36 @@
+package day10_Loops;
+
+import java.util.Scanner;
+
+public class WhileLoop03 {
+
+    /*
+ 		 Type java code by using while loop,
+         Write a program that prompts the user to input an integer.
+         It should then find the sum of the digits of that number.
+         123 ==> 1+2+3 = 6
+	 */
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter an integer to find the sum of the digits...");
+        int num = scan.nextInt();
+
+        if(num<0) {
+            num = -1*num;
+        }
+
+        int sum = 0;
+
+        while(num!=0) {
+            sum = sum + num%10;
+            num = num/10;
+        }
+
+        System.out.println(sum);
+
+        scan.close();
+
+    }
+}
