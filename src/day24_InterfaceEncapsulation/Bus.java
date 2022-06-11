4e5d4f5fba0f79902978e07bd35105a0887e2d98
ali ustundag <aliokanustundag@gmail.com>
2022-06-11 16:30:53 +0100

    day25

---
+++ src/day24_InterfaceEncapsulation/Bus.java
@@ -0,0 +1,34 @@
+package day24_InterfaceEncapsulation;
+
+
+public class Bus implements AirCondition{
+
+    @Override
+    public void electronicAc() {
+
+        System.out.println("The Bus AC is semi-electronic...");
+
+    }
+
+    @Override
+    public void climateAc() {
+
+        System.out.println("The Bus AC is not climate AC...");
+
+    }
+
+    @Override
+    public void bacteriaKiller() {
+
+        System.out.println("The Bus AC kills %85 bacteria...");
+
+    }
+
+    @Override
+    public void run() {
+
+        System.out.println("The Bus AC works perfectly...");
+
+    }
+
+}
---
+++ src/day24_InterfaceEncapsulation/Luxury.java
@@ -0,0 +1,7 @@
+package day24_InterfaceEncapsulation;
+
+public interface Luxury extends AirCondition{
+
+
+
+}
